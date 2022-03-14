f3a83f365ff5144fa8e58c1d7b339802fe747ab8
Rishabh Malviya <malviyarishabh73@gmail.com>
2022-03-14 11:41:31 +0530

    V1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Navbar} from './Components/Navbar';
+import '@fontsource/inter';
+import '@fontsource/space-grotesk';
+import {Hero} from './Components/hero';
+import {Working} from './Components/Working';
+import Panel from './Components/Panel';
+import AboutUs from './Components/AboutUs';
+import Question from './Components/Question';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen bg-red-200'>
+      <Navbar />
+      <Hero />
+      <Working />
+      {/* <AboutUs /> */}
+      <Question />
+      <Panel />
+      <Contact />
+      <Footer />
     </div>
   );
 }
